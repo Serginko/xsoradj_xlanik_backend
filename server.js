68d171d3ae846020e5a5571c94bec16a5f2db488
Serginko <jakubsorad@gmail.com>
2022-03-13 21:33:06 +0100

    Funkcny zakladny post a zakladny get

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 const databaseUri = process.env.DATABASE_URI
 
+app.use(express.json()) //https://stackoverflow.com/questions/18542329/typeerror-cannot-read-property-id-of-undefined
 
 MongoClient.connect(databaseUri, (err, client) => {
   if (err) return console.error(err)
@@ -16,26 +17,32 @@ MongoClient.connect(databaseUri, (err, client) => {
 
 
   app.post('/customers', (req, res) => {
-    /*customers.insertOne(req.body)
-      .then(result => {
-        console.log(result)
-      })
-      .catch(error => console.error(error))*/
+    
+      //todo validacia vstupnych dat
 
-      customers.insertOne( { item: "card", qty: 15 } ).then(() => {
+      customers.insertOne(req.body).then(() => {
         res.status(201).json(req.body);
       })
   })
+
+  
+
+  app.get('/customers', (req, res) => {
+    
+    //todo validacia vstupnych dat
+
+    customers.find().toArray(function(err, result) {
+      if (err) throw err;
+      //console.log(result);
+      res.status(201).json(result);
+    });
+})
 
 })
 
 app.get('/', function(req, res) {
   res.send('Hello World!')
 });
-
-app.post('/postik', (req, res) => {
-  console.log('Hellooooooooooooooooo!')
-})
 
 
 app.listen(port, function() {
